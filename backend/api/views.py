dbf810df7d1b3a681ef5f7060cf0da21af4fded5
Dan Kolan <dan.kolan@gmail.com>
2022-12-10 00:34:56 -0600

    views, urls endpoint; postgres healthcheck

---
+++ backend/api/views.py
@@ -0,0 +1,13 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from datetime import *
+
+@api_view(['GET'])
+def getData(request):
+    incident = {
+        'Datetime': datetime(2022, 12, 2, 7, 50, 22, 0),
+        'Case Number': '22M005979',
+        'Description': 'DOMESTIC VIOLENCE 3RD-CRIMINAL MISCHIEF 3RD',
+        'Location': '100 Block of MEADOW DR MAD'
+    }
+    return Response(incident)
