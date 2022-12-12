435640311c7dbe7dd953aab79b31e5b88a2f4829
Dan Kolan <dan.kolan@gmail.com>
2022-12-12 17:41:05 -0600

    added unit tests for Incident model and view_list)

--- backend/apis/tests.py
+++ backend/apis/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework.test import APIClient
+
+from .models import Incident
+from datetime import datetime
+
+class IncidentTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+
+    def create_incident(
+        self,
+        created = datetime.now(),
+        datetime = datetime(1937, 5, 6, 19, 25),
+        caseNumber = 'ABC123',
+        description = 'Gross Negligance and Hubris',
+        location = 'NAS Lakehurst, Manchester Township, New Jersey, U.S.'
+    ):
+        return Incident.objects.create(
+            created = created,
+            datetime = datetime,
+            caseNumber = caseNumber,
+            description = description,
+            location = location
+        )
+    
+    # Model
+    def test_incident_creation(self):
+        i = self.create_incident()
+        self.assertTrue(isinstance(i, Incident))
+        self.assertEqual(i.__unicode__(), i.caseNumber)
+
+    # View
+    def test_incident_list_view(self):
+        i = self.create_incident()
+        url = reverse('incident_list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        import pdb; pdb.set_trace()
+        self.assertIn(i.caseNumber, response.content.decode("utf-8"))
--- backend/apis/urls.py
+++ backend/apis/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from apis import views
 
 urlpatterns = [
-    path('incidents', views.IncidentList.as_view()),
-    path('incidents/<int:pk>', views.IncidentDetail.as_view())
+    path('incidents', views.IncidentList.as_view(), name='incident_list'),
+    path('incidents/<int:pk>', views.IncidentDetail.as_view(), name='incident')
 ]
