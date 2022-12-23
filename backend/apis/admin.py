b4946fc612985cf87454b63b0dfb184e27a595a5
Dan Kolan <dan.kolan@gmail.com>
2022-12-22 23:40:52 -0600

    added initial admin form for file uplaod, fixed PK db UUID

--- backend/apis/admin.py
+++ backend/apis/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.urls import path
+from django.shortcuts import render
+from django import forms
 
-# Register your models here.
+from .models import Incident
+
+class IncidentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'created',
+        'datetime',
+        'caseNumber',
+        'description',
+        'location'
+    )
+
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [path('upload-csv/', self.upload_csv)]
+        print(f'urls = {urls}')
+        print(f'new_urls = {new_urls}')
+        print(f'new_urls + urls = {new_urls + urls}')
+        return new_urls + urls
+
+    def upload_csv(self, request):
+        form = CsvImportForm()
+        data = {'form': form}
+        return render(request, 'admin/csv_upload.html', data)
+
+admin.site.register(Incident, IncidentAdmin)
+
+class CsvImportForm(forms.Form):
+    csv_upload = forms.FileField()
--- backend/apis/models.py
+++ backend/apis/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+import uuid
 
 class Incident(models.Model):
+    id = models.TextField(primary_key=True, default=uuid.uuid4, editable=False)
     created = models.DateTimeField(auto_now_add=True)
     datetime = models.DateTimeField()
     caseNumber = models.CharField(max_length = 15, blank=False, default='')
