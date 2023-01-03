e6387c9fad56ea0bd436d83f257f8b3c60a9240c
Dan Kolan <dan.kolan@gmail.com>
2023-01-03 16:59:45 -0600

    added unit tests for user permissions (read only, destructive calls = 403)

--- backend/apis/tests.py
+++ backend/apis/tests.py
@@ -113,4 +113,81 @@ class CustomIncidentAdmin(TestCase):
         self.assertEqual(str(incident_admin), 'apis.IncidentAdmin')
 
 class IncidentPermissions(TestCase):
-    pass
+    def setUp(self):
+        self.client = APIClient()
+
+        User = get_user_model()
+        self.user = User.objects.create_user(
+            username='mpruss',
+            email='mpruss@dzr.de',
+            password='hunter2')
+        self.client.force_login(user=self.user)
+
+    def create_incident(
+        self,
+        created = datetime.now(),
+        datetime = datetime(1937, 5, 6, 19, 25),
+        caseNumber = 'ABC123',
+        description = 'Gross Negligance and Hubris',
+        location = 'NAS Lakehurst, Manchester Township, New Jersey, U.S.'
+    ):
+        return Incident.objects.create(
+            created = created,
+            datetime = datetime,
+            caseNumber = caseNumber,
+            description = description,
+            location = location
+        )
+
+    def test_incident_creation_user_403(self):
+        incident = {
+            'datetime': datetime.now(),
+            'caseNumber': datetime(1937, 5, 6, 19, 25),
+            'description': 'ABC123',
+            'location': 'NAS Lakehurst, Manchester Township, New Jersey, U.S.'
+        }
+
+        url = reverse('incident_list')
+        response = self.client.put(url, data=incident)
+
+        self.assertEqual(response.status_code, 403)
+
+    # Views
+    def test_incident_list_view_user_200(self):
+        i = self.create_incident()
+        url = reverse('incident_list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(i.caseNumber, response.content.decode("utf-8"))
+
+    def test_create_read_incident_view_user_200(self):
+        i = self.create_incident()
+        url = reverse('incident', args=[i.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(i.caseNumber, response.content.decode("utf-8"))
+
+    def test_create_update_incident_view_user_403(self):
+        i = self.create_incident()
+        url = reverse('incident', args=[i.id])
+        response = self.client.get(url)
+
+        updated_incident = {
+            'datetime': i.datetime,
+            'caseNumber': i.caseNumber,
+            'description': "An updated description",
+            'location': i.location
+        }
+
+        response = self.client.put(url, data=updated_incident)
+
+        self.assertEqual(response.status_code, 403)
+
+    def test_delete_incident_view_user_403(self):
+        i = self.create_incident()
+        url = reverse('incident', args=[i.id])
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, 403)
