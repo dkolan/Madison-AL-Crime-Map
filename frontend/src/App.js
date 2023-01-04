a2b55013e4d7202f35f38e79d139c0ff342acdb3
Dan Kolan <dan.kolan@gmail.com>
2023-01-04 11:58:13 -0600

    corrected whitespace href formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,13 +25,13 @@ function App() {
         </Modal.Header>
         <Modal.Body>
           <p>
-            Welcome! This is a proof of concept on plotting crime incidents published by the
-            <a href="https://www.madisonal.gov/219/Police"> Madison City Police</a>.
+            Welcome! This is a proof of concept on plotting crime incidents published by the&nbsp;
+            <a href="https://www.madisonal.gov/219/Police">Madison City Police</a>.
             The data was provided by the <a href="https://www.madisonal.gov/Archive.aspx">Madison City Archive Center</a> as a PDF, parsed, and added to a RESTful API.
           </p>
           <p>
-            Unfortunately, the city chooses to present these reports in a format (PDF) which is not conducive to fair and open governmental practices. Please urge your
-            <a href="https://www.madisonal.gov/1195/Madison-Police-Citizens-Advisory-Committ"> Police Citizens Advisory Committee</a> representative to implement IT practices
+            Unfortunately, the city chooses to present these reports in a format (PDF) which is not conducive to fair and open governmental practices. Please urge your&nbsp;
+            <a href="https://www.madisonal.gov/1195/Madison-Police-Citizens-Advisory-Committ">Police Citizens Advisory Committee</a> representative to implement IT practices
             that allow for citizens to have fair and open access to this public data.
           </p>
           <p>
